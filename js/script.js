42bdc3aac41517bbfcc0cc4c1b170cfc4c8b275a
Naufal Rizkie Pribadi <naufalrizkiepribadi@MacBook-Pro-Naufal.local>
2022-08-31 14:23:39 +0700

    Menambahkan file project tugas

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const addBtn = document.getElementById("add-task-btn");
+console.log(addBtn);
+// menambahkan event add task button
+addBtn.addEventListener('click', function() {
+    const inputEl = document.getElementById("add-task-input");
+    console.log(inputEl);
+    inputEl.classList.toggle("hidden-task");
+})
+
+const dropBtn = document.getElementById("drop-btn");
+console.log((dropBtn));
+dropBtn.addEventListener('click', function() {
+    const dropEl = document.getElementById("dropdown-element");
+    console.log(dropEl);
+    dropEl.classList.toggle("hidden-content");
+})
