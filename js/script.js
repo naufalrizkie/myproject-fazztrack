8348b35116e9714fc02d7fb15c0b36872be69547
Naufal Rizkie <belajarbersamapaknopal@gmail.com>
2022-09-02 14:11:34 +0700

    menambahkan js line-through

--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
+// menambahkan event add task button
 const addBtn = document.getElementById("add-task-btn");
 console.log(addBtn);
-// menambahkan event add task button
 addBtn.addEventListener('click', function() {
     const inputEl = document.getElementById("add-task-input");
     console.log(inputEl);
     inputEl.classList.toggle("hidden-task");
 })
 
+//menambahkan event dropdown
 const dropBtn = document.getElementById("drop-btn");
 console.log((dropBtn));
-//menambahkan event dropdown
 dropBtn.addEventListener('click', function() {
     const dropEl = document.getElementById("dropdown-element");
     console.log(dropEl);
     dropEl.classList.toggle("hidden-content");
 })
 
-const arrowRightbtn = document.getElementById("arrow-right-btn");
-console.log(arrowRightbtn);
+//menambahkan event dropRightbtn
+// const arrowrighBtn = document.getElementById("arrow-right-btn");
+// console.log(arrowrightBtn);
+
+const checkboxBtn = document.getElementById("checkbox-button");
+console.log(checkboxBtn);
+checkboxBtn.addEventListener('click', function() {
+    const lineThrough = document.getElementById("demo");
+    lineThrough.style.textDecoration = "line-through";
+})
+
+// function myFunction() {
+//     document.getElementById("demo").style.textDecoration = "line-through"
+// }
