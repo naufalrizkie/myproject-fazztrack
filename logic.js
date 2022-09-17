dce8f436542796828a0962caa7fc52a04d024c73
Naufal Rizkie <belajarbersamapaknopal@gmail.com>
2022-09-17 13:37:41 +0700

    nambahin no 8,9,10 create logic star

--- logic.js
+++ logic.js
@@ -119,3 +119,52 @@ function trianglerightStar(n) {
 trianglerightStar(8);
 
 console.log("jawaban no 8");
+function hourglassVer(n) {
+    let hasil = "";
+    for (let x = 0; x <= n; x++) {
+        for (let y = 0; y <= n; y++) {
+            if (x==0 || x==1 && y>0 && y<8 || x==2 && y>1 && y<7 || x==3 && y>2 && y<6 || x==4 && y==4 || x==5 && y>2 && y<6 || x==6 && y>1 && y<7 || x==7 && y>0 && y<8 || x==8) {
+                hasil += "*";
+            } else {
+                hasil += " ";
+            }
+        }
+        hasil += "\n";
+    }
+    console.log(hasil);
+}
+hourglassVer(8);
+
+console.log("jawaban no 9");
+function hourglassHor(n) {
+    let hasil = "";
+    for (let x = 0; x <= n; x++) {
+        for (let y = 0; y <= n; y++) {
+            if (y==0 || y==1 && x>0 && x<8 || y==2 && x>1 && x<7 || y==3 && x>2 && x<6 || y==4 && x==4 || y==5 && x>2 && x<6 || y==6 && x>1 && x<7 || y==7 && x>0 && x<8 || y==8) {
+                hasil += "*";
+            } else {
+                hasil += " ";
+            }
+        }
+        hasil += "\n";
+    }
+    console.log(hasil);
+}
+hourglassHor(8);
+
+console.log("jawaban no 10");
+function lastNum(n) {
+    let hasil = "";
+    for (let x = 0; x <= n; x++) {
+        for (let y = 0; y <= n; y++) {
+            if (y==0 && x<5 || y==1 && x<5 && x>0 || y==2 && x<5 && x>1 || y==3 && x<5 && x>2 || y==4 && x<5 && x>3 || y==5 && x>3 && x<6 || y==6 && x>3 && x<7 || y==7 && x>3 && x<8 || y==8 && x>3 && x<9) {
+                hasil += "*";
+            } else {
+                hasil += " ";
+            }
+        }
+        hasil += "\n";
+    }
+    console.log(hasil);
+}
+lastNum(8);
