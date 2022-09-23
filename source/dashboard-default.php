705d9e6c6b2a4b31446094b4296b34e8fe9b43c0
Naufal Rizkie <belajarbersamapaknopal@gmail.com>
2022-09-23 08:08:15 +0700

    date Button orange change to grey and grey change to orange

--- source/dashboard-default.php
+++ source/dashboard-default.php
@@ -1,6 +1,7 @@
 
 <?php
 include '../php/connection.php';
+// include '../php/create.php';
 $user_id = 1;
 $asset = "../assets/";
 ?>
@@ -55,48 +56,48 @@ $asset = "../assets/";
                                 <p class=text-tambah-tugas>Tambah Tugas</p>
                             </button>
                             <div class="dropdown">
-                                <!-- dropdown button orange -->
-                                <button class="dropbtn" id="drop-btn">
+                                <!-- dropdown button date orange -->
+                                <button class="dropbtn-date-orange hidden-btn-date-orange" id="drop-btn">
                                     <p class="text-bydate">By Tanggal</p>
                                     <img src="<?php echo $asset; ?>svg/Arrow-Down-2.svg" alt="arrow-down-2" class="arrow-down-orange">    
                                 </button>                                
-                                <!-- <div class="hidden-content">
-                                    dropdown button abu-abu
-                                    <button class="dropdownbtn hidden-el">
-                                        <p class="ext-bydate-abu">By Tanggal</p>
-                                        <img src="..//assets/svg/Arrow - Up.svg" alt="arrow-up" class="arrow-up">
-                                    </button> -->
-                                    <div class="dropdown-content hidden-content" id="dropdown-element">
-                                        <div class="ctn1"><p class="text-by-tgl">By Tanggal</p><input type="radio" name="sortby" class="radio-bttn"></div>
-                                        <div class="ctn2"><p class="text-by-time">By Time</p><input type="radio" name="sortby" class="radio-bttn"></div>
-                                        <div class="ctn3"><p class="text-terbaru">Terbaru</p><input type="radio" name="sortby" class="radio-bttn"></div>
-                                    </div>
-                                <!-- </div> -->
+                                <!-- dropdown button date abu-abu -->
+                                <button class="dropbtn-date-grey hidden-btn-date-grey" id="date-btn-grey">
+                                    <p class="text-bydate-abu">By Tanggal</p>
+                                    <img src="..//assets/svg/Arrow-Up.svg" alt="arrow-up-grey" class="arrow-up-date-grey">
+                                </button>
+                                <div class="dropdown-content hidden-content" id="dropdown-element">
+                                    <div class="ctn1"><p class="text-by-tgl">By Tanggal</p><input type="radio" name="sortby" class="radio-bttn"></div>
+                                    <div class="ctn2"><p class="text-by-time">By Time</p><input type="radio" name="sortby" class="radio-bttn"></div>
+                                    <div class="ctn3"><p class="text-terbaru">Terbaru</p><input type="radio" name="sortby" class="radio-bttn"></div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="container-task">
                     <div class="create hidden-task" id="add-task-input">
-                        <div class="create-tambah-tugas">
-                            <div class="round">
-                                <input type="checkbox" name="checkbox" unchecked id="checkbox-create" class="checkbox-button" onclick="myFunction ('myCreate')">
-                                <label for="checkbox-create"></label>
-                                <input type="text" class="masukkan-nama-tugas" placeholder="Masukkan nama tugas">
+                        <form action="../php/create.php" method="post">
+                            <div class="create-tambah-tugas">
+                                <div class="round">
+                                    <input type="checkbox" name="checkbox" unchecked id="checkbox-create" class="checkbox-button" onclick="myFunction ('myCreate')">
+                                    <label for="checkbox-create"></label>
+                                    <input type="text" name="task_name" class="masukkan-nama-tugas" placeholder="Masukkan nama tugas">
+                                </div>
                             </div>
-                        </div>
-                        <div class="input-desc-task">
-                            <img src="<?php echo $asset; ?>svg/menu.svg" alt="menu" class="menu">
-                            <input type="text" class="desc" placeholder="Deskripsi Tugas (Optional)">                        
-                        </div>
-                        <div class="input-due-date">
-                            <img src="<?php echo $asset; ?>svg/Calendar.svg" alt="Calendar" class="calendar">
-                            <input type="datetime-local" class="tanggal-waktu" placeholder="Tanggal & Waktu">                        
-                        </div>
-                        <button class="button-submit-task" id="submit-task-btn" type="submit">
-                            <p class=text-submit-task>Submit</p>
-                        </button>
-                    </div>
+                            <div class="input-desc-task">
+                                <img src="<?php echo $asset; ?>svg/menu.svg" alt="menu" class="menu">
+                                <input type="text" name="task_desc" class="desc" placeholder="Deskripsi Tugas (Optional)">                        
+                            </div>
+                            <div class="input-due-date">
+                                <img src="<?php echo $asset; ?>svg/Calendar.svg" alt="Calendar" class="calendar">
+                                <input type="datetime-local" name="task_due_date" class="tanggal-waktu" placeholder="Tanggal & Waktu">                        
+                            </div>
+                                <button class="button-submit-task" id="submit-task-btn" type="submit" action="Submit">
+                                    <p class=text-submit-task>Submit</p>
+                                </button>
+                            </div>
+                        </form>
                     <div class="task">
                         <?php
                             $link = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
@@ -120,6 +121,7 @@ $asset = "../assets/";
                                             </div>
                                             <div>
                                                 <img src="<?php echo $asset; ?>svg/Arrow-Down-1.svg" alt="arrow-down-1" class="arrow-down-black" onclick="hideButton('subtask-btn-<?php echo $row->task_id; ?>')" style="cursor: pointer">
+                                                <img src="<?php echo $asset; ?>svg/Arrow-Up-1.svg" alt="arrow-up-grey" class="arrow-up-grey" onclick="hideButton('subtask-btn-<?php echo $row->task_id; ?>')" style="cursor: pointer">
                                             </div>
                                         </div>
                                         <?php if ($row->description) { ?>
@@ -171,10 +173,10 @@ $asset = "../assets/";
                             <!-- border garis -->
                         </div> 
                         <div class="task-done-title">
-                            <img src="<?php echo $asset; ?>svg/Arrow-Right-1.svg" alt="arrow-right-1" class="arrow-right-1" id="arrow-right-btn">
+                            <img src="<?php echo $asset; ?>svg/Arrow-Right-1.svg" onclick="taskCompleted('task-completed')" alt="arrow-right-1" class="arrow-right-1" id="arrow-right-btn">
                             <p>Terselesaikan (3 tugas)</p>
                         </div>
-                        <div class="task-done-list hidden-task-done">
+                        <div class="task-done-list hidden-task-done" id="task-completed">
                         <?php
                             $link = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
                             if($link) {
@@ -188,10 +190,10 @@ $asset = "../assets/";
                                     while($row = $result->fetch_object()) {
                                         ?>
                                         <div class="completed">
-                                            <div class="round">
+                                            <div class="round round-completed">
                                                 <input type="checkbox" name="checkbox" checked id="checkbox-done-<?php echo $row->task_id; ?>" class="checkbox-button" onclick="myFunction ('task-done-<?php echo $row->task_id; ?>')">
                                                 <label for="checkbox-done-<?php echo $row->task_id; ?>"></label>
-                                                <p class="requierements-detail" id="task-done-<?php echo $row->task_id; ?>"><?php echo $row->task_name; ?></p>
+                                                <p class="completed-list" id="task-done-<?php echo $row->task_id; ?>"><?php echo $row->task_name; ?></p>
                                             </div>
                                         </div>
                                         <?php
